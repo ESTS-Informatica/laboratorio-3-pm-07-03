d3b56f89a9b802938532abee63d5bae221abff2f
Bozoed <202300253@estudantes.ips.pt>
2024-04-03 08:53:45 +0100

    Nivel 1

---
+++ GroundTransportation.java
@@ -0,0 +1,11 @@
+
+/**
+ * Escreva uma descrição da classe GroundTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class GroundTransportation extends Transport
+{
+    private String licensePlate;
+}
